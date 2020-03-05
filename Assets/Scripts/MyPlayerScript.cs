1c4cf41ff94561a7545ec655fd2004451b602afc
Priyank Jain <fly2priyank@gmail.com>
2020-03-05 00:58:57 -0800

    adding input to move cube left / right

---
+++ Assets/Scripts/MyPlayerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MyPlayerScript : MonoBehaviour
+{
+    //public void OnFire(InputAction.CallbackContext context)
+    //{
+    //}
+
+    //public void OnMove(InputAction.CallbackContext context)
+    //{
+    //    var value = context.ReadValue<Vector2>();
+    //}
+
+    
+public void OnMove(InputValue value)
+    {
+        transform.Translate(value.Get<Vector2>().x, 0, 0);
+    }
+
+    public void OnFire()
+    {
+        Debug.Log("OnFire");
+    }
+}
